6e6e79dffb44e12d5a6b3f8cf21ab179e603b109
Alwin Kurian <imalwinkurian4@gmail.com>
2024-11-16 21:15:56 +0530

    MYSQL + HikariCP

--- src/main/java/com/example/demo/entity/Item.java
+++ src/main/java/com/example/demo/entity/Item.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,18 +15,21 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "ITEM_DTLS")
 public class Item {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 
 	@NotNull
 	@Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+	@Column(name = "name")
 	private String name;
 
 	@Min(value = 1, message = "Price must be greater than or equal to 1")
+	@Column(name = "price")
 	private double price;
 
 }
