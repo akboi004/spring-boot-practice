7240f47cc6613be2ac389eadf1beb1501db6228c
Alwin Kurian <imalwinkurian4@gmail.com>
2024-12-04 21:12:16 +0530

    Self Assigned SSL

---
+++ src/main/java/com/example/demo/controller/SecuredServerController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecuredServerController {
+
+	@GetMapping("/secured")
+	public String secured() {
+		System.out.println("Inside secured()");
+		return "Hello user !!! : " + new Date();
+	}
+}
