2cdb5a31d2f507ce6bcf8719b9dfe3fbdcb7e7bd
Alwin Kurian <imalwinkurian4@gmail.com>
2024-12-10 21:18:53 +0530

    Logging

--- src/main/java/com/example/demo/controller/CorsController.java
+++ src/main/java/com/example/demo/controller/CorsController.java
@@ -6,15 +6,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @CrossOrigin(methods = RequestMethod.GET, maxAge = 1800, allowedHeaders = "*", origins = "*")
 @RestController
 public class CorsController {
 
 	@GetMapping("/fetchCorsUsers")
 	public Object fetchUsers() {
+		log.info("/fetchCorsUser endpoint started");
 		String url = "https://jsonplaceholder.typicode.com/users";
 		RestTemplate restTemplate = new RestTemplate();
 
+		log.info("RESPONSE ->{}", restTemplate.getForObject(url, Object.class));
 		return restTemplate.getForObject(url, Object.class);
 	}
 }
